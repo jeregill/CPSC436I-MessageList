021f9d30c23a8990f933c9d462c65db81f376d21
jeregill <43596101+jeregill@users.noreply.github.com>
2020-06-25 10:11:59 -0700

    added spinner component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import TopNav from "./components/common/TopNav/TopNav";
 import PostsListContainer from "./components/feed/PostCard/PostsListContainer";
 import InputCardContainer from "./components/feed/InputCard/InputCardContainer";
+import axios from 'axios';
+import {postsURI, usersURI} from "./store/endpoints";
+import Spinner from "./components/spinner/spinner";
 
 function App() {
-    return (
-      <div>
-        <div id="header">
-             <TopNav />
-        </div>
-        <div id='content'>
+    const [postsLoaded, loadPosts] = useState(
+        false
+    )
+    const [usersLoaded, loadUsers] = useState(
+        false
+    )
 
-          <InputCardContainer />
-          <PostsListContainer />
-         </div>
-          <div className="empty-space"></div>
-      </div>
-  );
+    useEffect(() => {
+        async function fetchPosts() {
+            let response = await axios(postsURI);
+            loadPosts(true);
+            console.log(response);
+        }
+
+        async function fetchUsers(){
+            let response = await axios(usersURI);
+            loadUsers(true);
+            console.log(response);
+
+        }
+
+        fetchPosts();
+        fetchUsers();
+    }, [])
+
+    if(postsLoaded && usersLoaded){
+        return (
+            <div>
+                <div id="header">
+                    <TopNav />
+                </div>
+                <div id='content'>
+                    <InputCardContainer />
+                    <PostsListContainer />
+                </div>
+                <div className="empty-space"></div>
+            </div>
+        );
+    } else { return (
+            <div>
+                <Spinner/>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ frontend/src/store/endpoints.ts
@@ -0,0 +1,2 @@
+export const usersURI = 'http://localhost:9000/users';
+export const postsURI = 'http://localhost:9000/posts';
